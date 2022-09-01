820b6c3b84390ca96b18a29efc379d2a89e8a4d1
Phu Mai <s3927049@rmit.edu.vn>
2022-09-01 15:01:06 +0700

    add login page

--- README.txt
+++ README.txt
@@ -1,2 +1,5 @@
 source images:
 - https://www.behance.net/gallery/138075205/Nimbus?tracking_source=search_projects&fbclid=IwAR3nbeizEBXV24tl3g_I8_cSIKrSAhebftJD9XBijcyr-9e9cnl5shbxSOM
+
+Icon:
+http://themify.me/themify-icons
--- login.php
+++ login.php
@@ -5,41 +5,51 @@ include_once 'header.php';
 <div class="signin-container">
 <div class="form-container">
 
+<h1>Logo.</h1>
  <form action="includes/login.inc.php" method="post">
-   <div>
+   <div class="login">
      <input type="text" name="username" placeholder="Username">
    </div>
-   <div>
+   <div class="login">
      <input type="password" class="" name="pwd" placeholder="Password">
    </div>
-   <button type="submit" class="" name="submit">Submit</button>
+   <div class="submit-container">
+
+  <span><a href="#">Forgotten password?</a></span>
+   <button type="submit" class="" name="submit">Sign in</button>
+
+ </div>
+ <?php
+  if(isset($_GET['error'])) {
+    echo '<div class="error">';
+    if($_GET['error']=='emptyUsername') {
+      echo '<p>Please enter your username</p>';
+    }
+
+    if($_GET['error']=='emptyPassword') {
+      echo '<p>Please enter your password</p>';
+    }
+
+    if($_GET['error']=='usernameNotExisted') {
+      echo '<p>Username does not exist</p>';
+    }
+
+    if($_GET['error']=='wrongLogin') {
+      echo '<p>Wrong username or password</p>';
+    }
+    echo '</div>';
+  }
+ ?>
+
  </form>
+
+ <div class="signup">
+   <a href="signup.php">Create New Account</a>
+
+ </div>
 </div>
 </div>
-<?php
- if(isset($_GET['error'])) {
-   if($_GET['error']=='emptyUsername') {
-     echo '<p>Please enter your username</p>';
-   }
 
-   if($_GET['error']=='emptyPassword') {
-     echo '<p>Please enter your password</p>';
-   }
-
-   if($_GET['error']=='usernameNotExisted') {
-     echo '<p>Username does not exist</p>';
-   }
-
-   if($_GET['error']=='wrongLogin') {
-     echo '<p>Wrong username or password</p>';
-   }
- }
-
-
-
-
-
-?>
 
 
 
