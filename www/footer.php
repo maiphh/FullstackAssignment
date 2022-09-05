ceed03ade8d6d2c1b6027a24b7f3c41466e3acf4
Khoa Nguyen <s3891524@rmit.edu.vn>
2022-09-05 19:59:55 +0700

    Added filters.php and product-display.php

--- www/footer.php
+++ www/footer.php
@@ -1,9 +1,9 @@
 <body>
-  <div class="footer">
-   <div class="wrapper">
-      <p class="text-center">2020 All rights reserved, Lazada Prototype. Developed by <a href="#">Khoa Nguyen</a>, 
-      <a href="#">Phu Mai</a>, <a href="#">Ngoc Linh</a>, <a href="#">Long Nguyen</a></p>
-   </div>
-  </div> 
+  <div class="footer wrapper">
+    <p class="copyright text-center">2022 All rights reserved, Lazada Prototype. Developed by <a href="#">Khoa Nguyen</a>,
+      <a href="#">Phu Mai</a>, <a href="#">Ngoc Linh</a>, <a href="#">Long Nguyen</a>
+    </p>
+  </div>
 </body>
+
 </html>
--- www/index.php
+++ www/index.php
@@ -2,6 +2,14 @@
 <?php include_once 'header.php' ?>
 <!-- Footer ends here -->
 
+<!-- Search and Filter starts here -->
+<?php include_once 'filters.php' ?>
+<!-- Search and Filter ends here -->
+
+<!-- Product display starts here -->
+<?php include_once 'products_display.php' ?>
+<!-- Product display ends here -->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <!-- Footer starts here -->
