ae9567d6827810002238a9586f1d1945bc6c81de
Khoa Nguyen <s3891524@rmit.edu.vn>
2022-09-05 19:28:42 +0700

    Include Footer.php into index.php

--- www/index.php
+++ www/index.php
@@ -1,13 +1,9 @@
-<?php
-include_once 'header.php'
- ?>
+<!-- Header starts here -->
+<?php include_once 'header.php' ?>
+<!-- Footer ends here -->
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-
-
-
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
-</html>
+<!-- Footer starts here -->
+<?php include_once 'footer.php' ?>
+<!-- Footer ends here -->
