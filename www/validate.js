cd7a7603aa9a56de8f07bf2ce8ed14a68214c580
Phu Mai <s3927049@rmit.edu.vn>
2022-09-07 20:21:10 +0700

    responsive product detail

--- www/validate.js
+++ www/validate.js
@@ -1,18 +1,40 @@
-const username = document.getElementById('username')
-const pwd = document.getElementById('pwd')
-const pwdrp = document.getElementById('pwdrp')
-const form = document.getElementById('signup-form')
+// const username = document.getElementById('username')
+// const pwd = document.getElementById('pwd')
+// const pwdrp = document.getElementById('pwdrp')
+// const form = document.getElementById('signup-form')
+//
+// form.addEventListener('submit',(e)=> {
+//   if(!(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/.test(username.value))) {
+//     window.location.href = "signup.customers.php?error=invalidUsername"
+//   }
+//   if(!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/.test(pwd.value))) {
+//     window.location.href = "signup.customers.php?error=invalidPassword"
+//   }
+//   if(!(pwd!==pwdrp)) {
+//     window.location.href = "signup.customers.php?error=passwordDontMatch"
+//   }
+//
+//
+// })
 
-form.addEventListener('submit',(e)=> {
+var username = document.getElementById('username')
+var pwd = document.getElementById('pwd')
+var pwdrp = document.getElementById('pwdrp')
+var form = document.getElementById('signup-form')
+
+
+function check() {
   if(!(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/.test(username.value))) {
     window.location.href = "signup.customers.php?error=invalidUsername"
+    return false;
   }
-  if(!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/.test(pwd.value))) {
+  else if(!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/.test(pwd.value))) {
     window.location.href = "signup.customers.php?error=invalidPassword"
+    return false;
   }
-  if(!(pwd!==pwdrp)) {
+  else if(pwd.value!==pwdrp.value) {
     window.location.href = "signup.customers.php?error=passwordDontMatch"
+    return false;
   }
-
-
-})
+  return true;
+}
