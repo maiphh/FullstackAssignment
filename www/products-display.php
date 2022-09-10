2abd1f7683b28d13e8453e7f09f80b7e22418cec
Khoa Nguyen <s3891524@rmit.edu.vn>
2022-09-10 22:16:10 +0700

    minimal clean up

--- www/products-display.php
+++ www/products-display.php
@@ -1,24 +1,22 @@
-
-
   <h1>Products</h1>
   <div class="product-display">
 
     <?php
-    if(isset($_SESSION['type'])) {
-    if($_SESSION['type']=='C'){
-    foreach ($products as $product) {
-      displayProduct($product['0'], $product['1'], $product['2'], $product['3'], $product['4'],$product['5']);
-    }}
-    else {
+    if (isset($_SESSION['type'])) {
+      if ($_SESSION['type'] == 'C') {
+        foreach ($products as $product) {
+          displayProduct($product['0'], $product['1'], $product['2'], $product['3'], $product['4'], $product['5']);
+        }
+      } else {
+        foreach ($products as $product) {
+          viewProduct($product['0'], $product['1'], $product['2'], $product['3'], $product['4'], $product['5']);
+        }
+      }
+    } else {
       foreach ($products as $product) {
-        viewProduct($product['0'], $product['1'], $product['2'], $product['3'], $product['4'],$product['5']);
-    }}
-  }
-  else {
-    foreach ($products as $product) {
-      viewProduct($product['0'], $product['1'], $product['2'], $product['3'], $product['4'],$product['5']);
+        viewProduct($product['0'], $product['1'], $product['2'], $product['3'], $product['4'], $product['5']);
+      }
     }
-  }
     ?>
 
   </div>
